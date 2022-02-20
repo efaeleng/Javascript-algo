4703a68ea01a05a80a2f19947241ba6506414fe5
Efa-iwa Eleng <gistend@gmail.com>
2022-02-20 18:44:25 +0100

    solution 03

---
+++ 03.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program to compute the sum and product of an array of integers
+
+const int =[2, 4, 6, 8, 10, 14, 16];
+let sum = 0;
+let product = 1;
+
+for(let i=0; i < int.length;i++){
+   sum+= int[i] 
+    product*= int[i]}
+
+console.log(`the sum is ${sum} and the product: ${product}`)
