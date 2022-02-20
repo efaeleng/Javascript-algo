08d83d1ff16e3bc6023810da263429ac0da89195
Efa-iwa Eleng <gistend@gmail.com>
2022-02-20 22:21:37 +0100

    fixed solution 04

--- 04.js
+++ 04.js
@@ -1,4 +1,6 @@
 // Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity)
 
-const duplicates = [1,2,2,5,5,6, 'hello','hello']
-console.log([...new Set(duplicates)])
+let duplicates = ['A', 'B', 'A', 'C', 'B' , 5 , 7, 40, 'A'];
+let unique = [...new Set(duplicates)];
+
+console.log(unique);
